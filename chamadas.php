feddde648f9b9f90e0f834c12187fcddd50cde38
gabryel <gabryel.dias@academico.ifpb.edu.br>
2024-10-24 15:31:36 -0300

    modificação de pacientes

--- chamadas.php
+++ chamadas.php
@@ -3,29 +3,29 @@
   <div class="row">
     <div class="informativeField">
       <div>
-        <p>Pacientes atendidos</p>
+        <p>Atendidos</p>
         <p class="labelText">Total</p>
       </div>
       <div class="informativeNumber">
-        <p class="number">0</p>
+        <p class="number" id="total">0</p>
       </div>
     </div>
     <div class="informativeField">
       <div>
-        <p>Pacientes aguardando</p>
+        <p>Aguardando</p>
         <p class="labelText">Convencional</p>
       </div>
       <div class="informativeNumber">
-        <p class="number">0</p>
+        <p class="number" id="qtdConvencional">10</p>
       </div>
     </div>
     <div class="informativeField">
       <div>
-        <p>Pacientes aguardando</p>
+        <p>Aguardando</p>
         <p class="labelText">Preferencial</p>
       </div>
       <div class="informativeNumber">
-        <p class="number">0</p>
+        <p class="number" id="qtdPreferencial">20</p>
       </div>
     </div>
   </div>
