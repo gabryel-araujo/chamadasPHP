d45bd407cb79a71b613a3b181427740924114ce1
gabryel <gabryel.dias@academico.ifpb.edu.br>
2024-10-24 15:32:20 -0300

    inserido regras de romoção

--- index.php
+++ index.php
@@ -16,10 +16,22 @@
     <?php include 'aside.php'; ?>
     <div class="divCentral">
       <?php include 'chamadas.php' ?>
-      <?php include 'medicos.php' ?>
+      <div class="card flex ">
+        <div class="divConvencional flex justify-center">
+          <h2 class="underline">Convencional</h2>
+        </div>
+        <div class="divPreferencial flex justify-center">
+          <h2 class="underline">Preferencial</h2>
+        </div>
+      </div>
 
+      <div class="flex justify-center items-center gap-3">
+        <button class="convencionalButton" id="convencionalButton" onclick="decrementarConvencional()">Convencional</button>
+        <button class="preferencialButton" id="preferencialButton" onclick="decrementarPreferencial()">Preferencial</button>
+      </div>
     </div>
   </main>
+  <script src="main.js"></script>
 </body>
 
 </html>
