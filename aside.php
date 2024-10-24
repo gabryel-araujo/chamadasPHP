02504988d17cb7f767e42a57f5278484642ac4ba
gabryel <gabryel.dias@academico.ifpb.edu.br>
2024-10-24 15:31:15 -0300

    inserção da logo

--- aside.php
+++ aside.php
@@ -4,16 +4,9 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
 ?>
 
 <aside>
-  <p class="teste">Logo</p>
+  <img src="Queue-bro.png" alt="imagem de fila">
   <div class="<?= $page == 'dashboard' ? 'selectedButtonArea' : 'buttonArea' ?>">
     <i class="fa-solid fa-chart-simple"></i>
     <p>Dashboard</p>
   </div>
-
-  <div class="<?= $page == 'medicos' ? 'selectedButtonArea' : 'buttonArea' ?>">
-    <i class="fa-solid fa-user-doctor"></i>
-    <p>Médicos</p>
-  </div>
 </aside>
-
-
