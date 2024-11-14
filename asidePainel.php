7f7457abf8bff37b95d32f132a60d1552e140a26
gabryel <gabryel.dias@academico.ifpb.edu.br>
2024-11-14 19:13:29 -0300

    barra lateral do ainel de chamadas

---
+++ asidePainel.php
@@ -0,0 +1,17 @@
+<?php
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+?>
+
+<aside>
+  <div class="horario">
+    <h1>12:30</h1>
+  </div>
+
+  <div class="chamadas">
+    <h3>Últimas chamadas:</h3>
+    <div class='grid-item-convencional'>C1</div>
+    <div class='grid-item-convencional'>C2</div>
+    <div class='grid-item-preferencial'>P1</div>
+  </div>
+</aside>
