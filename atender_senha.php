feb7c5beecddd997491d32f2546e1906d5bb69a7
gabryel <gabryel.dias@academico.ifpb.edu.br>
2024-11-14 19:13:43 -0300

    atendimento de senhas

--- atender_senha.php
+++ atender_senha.php
@@ -1,7 +1,16 @@
 <?php
 include_once "conexao.php";
 
-$query = "UPDATE senha SET sits_senha_id = 3 WHERE nome_senha like 'C1'";
-$result = $conn->prepare($query);
+$senha = filter_input(INPUT_GET, "senha", FILTER_DEFAULT);
 
-$result->execute();
+if ($senha) {
+  $query = "UPDATE senha SET sits_senha_id = 3 WHERE nome_senha like :nome_senha";
+  $result = $conn->prepare($query);
+
+  $result->bindParam(":nome_senha", $senha);
+  if ($result->execute()) {
+    echo "Atualização executada com sucesso";
+  } else {
+    echo "Erro ao atualizar.";
+  }
+}
